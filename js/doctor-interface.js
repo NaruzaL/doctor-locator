8d066e1f431cd8fc1fb1381e3afec772042737db
Pete Lazuran <pdlazuran@gmail.com>
2017-06-30 15:51:43 -0700

    add css styling

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,7 +2,7 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctors = function(allDoctors){
   allDoctors.forEach(function(doctor){
-    $('#responses').append('<p>' + doctor.name + '</p>');
+    $('#responses').append('<p><strong>' + doctor.name + '</strong></p>');
     $('#responses').append('<p>' + "Address:" + doctor.address + '</p>');
     $('#responses').append('<p>' + "Phone:" + doctor.phone + '</p><br>');
   });
@@ -11,8 +11,10 @@ var displayDoctors = function(allDoctors){
 
 $(document).ready(function(){
   var doctorObject = new Doctor();
+  $('#responses').hide();
   $("form#symptom").submit(function(event){
     event.preventDefault();
+    $('#responses').show();
     $('#responses').empty();
     var medicalIssue = $("#medicalIssue").val();
     doctorObject.getDoctors(medicalIssue, displayDoctors);
