8070364514c81a82accf77560ed9916c2e1c30c1
Pete Lazuran <pdlazuran@gmail.com>
2017-06-30 10:43:17 -0700

    recieve JSON from API, pretty printing to narrow down what to display on page

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,15 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+
+var displayDoctors = function(allDoctors){
+  $('#responses').text(allDoctors);
+};
+
+
+$(document).ready(function(){
+  var doctorObject = new Doctor();
+  $("form#symptom").submit(function(event){
+    event.preventDefault();
+    var medicalIssue = $("#medicalIssue").val();
+    doctorObject.getDoctors(medicalIssue);
+  });
+});
