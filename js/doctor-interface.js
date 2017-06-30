fb6ee55d8163d353daee9f4dd4665d31b36bb4cf
Pete Lazuran <pdlazuran@gmail.com>
2017-06-30 13:40:48 -0700

    print results to html, many duplicate results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,7 +1,12 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctors = function(allDoctors){
-  $('#responses').text(allDoctors);
+  allDoctors.forEach(function(doctor){
+    $('#responses').append('<p>' + doctor.name + '</p>');
+    $('#responses').append('<p>' + "Address:" + doctor.address + '</p>');
+    $('#responses').append('<p>' + "Phone:" + doctor.phone + '</p><br>');
+
+  });
 };
 
 
@@ -9,7 +14,8 @@ $(document).ready(function(){
   var doctorObject = new Doctor();
   $("form#symptom").submit(function(event){
     event.preventDefault();
+    $('#responses').empty();
     var medicalIssue = $("#medicalIssue").val();
-    doctorObject.getDoctors(medicalIssue);
+    doctorObject.getDoctors(medicalIssue, displayDoctors);
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,22 +1,29 @@
 var apiKey = require('./../.env').apiKey;
 
-Doctor = function(){
+Doctor = function(doctorName, address, phone){
   this.name = doctorName;
-  this.insurance = [];
   this.address = address;
   this.phone = phone;
-  this.hours = hours;
 };
 
 
-Doctor.prototype.getDoctors = function(medicalIssue) {
+Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key='+apiKey)
    .then(function(result) {
-      console.log(result);
-      console.log(JSON.stringify(result.data));
+     var allDoctors = [];
+     for (var i = 0; i < result.data[0].practices.length; i++){
+       var newDoctor = new Doctor(doctorName = result.data[0].practices[i].name, address =  result.data[0].practices[i].visit_address.street + ", " + result.data[0].practices[i].visit_address.city + ", " + result.data[0].practices[i].visit_address.zip , phone = result.data[0].practices[i].phones[0].number);
+       allDoctors.push(newDoctor);
+     }
+     displayDoctors(allDoctors);
+      // console.log(allDoctors[0], allDoctors[1]);
+      // console.log(JSON.stringify(result));
+      // console.log(JSON.stringify(result.data));
+      // console.log(JSON.stringify(result.data[0].practices.length));
+      // console.log(JSON.stringify(result.data[0].practices[0].name));
     })
    .fail(function(error){
-      console.log("fail");
+      $('#responses').text(error.responseJSON.message);
     });
 };
 
